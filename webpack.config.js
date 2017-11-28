dc908aacaa3b6eb096a33e260b914912c7a24280
rlatjdfo112 <rlatjdfo112@naver.com>
2017-11-28 23:25:25 +0900

    [UPDATE] package, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,41 @@ module.exports = {
     },
     output: {
         path: DEST,
-        filename: 'app.js',
+        filename: 'bundle.js',
         publicPath: '/js/',
         library: "MainApp"
     },
+    devServer: {
+            hot: true,
+            inline: true,
+            host: '0.0.0.0',
+            port: 7777,
+            contentBase: DEST
+    },
     module: {
         loaders: [
-            {
-                test: /\.jsx?$/,
-                loaders: ['babel'],
-                include: SRC
-            }
+                 {
+                 test: /\.js$/,
+                 loader: ['react-hot-loader', 'babel-loader?' + JSON.stringify({
+                     cacheDirectory: true,
+                     presets: ['es2015', 'react'],
+                     plugins: ["transform-object-rest-spread"]
+                 })],
+                 exclude: /node_modules/
+             },
+             {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin([
+             {
+                 from: 'node_modules/monaco-editor/min/vs',
+                 to: 'vs',
+             }
+         ])
+    ]
 };
